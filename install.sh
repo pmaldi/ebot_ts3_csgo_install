79c6d8d449326a03caa2326cdabd60625e8d2fd2
pmaldi <patrice.maldi@gmail.com>
2020-02-01 23:03:08 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -15,17 +15,17 @@ cd /home/install-scripts
 wget --no-check-certificate https://raw.githubusercontent.com/Flegma/eBot-install-script/master/ebot-install.sh && chmod +x ebot-install.sh && ./ebot-install.sh #we need the --no-check-certificate here because we are using old ssl libs and it will fail to verify ssl certificates
 rm -rf /home/ebot/ebot-web/web/installation #we need to remove installation directory in order for ebot to work
 #and now ts3 server
-wget --no-check-certificate https://files.teamspeak-services.com/releases/server/3.5.0/teamspeak3-server_linux_amd64-3.5.0.tar.bz2 #we need the --no-check-certificate here because we are using old ssl libs and it will fail to verify ssl certificates
-tar -xjvf teamspeak3-server_linux_amd64-3.5.0.tar.bz2
-mv teamspeak3-server_linux_amd64 /home/flegma/teamspeak
-touch /home/flegma/teamspeak/.ts3server_license_accepted
-#if you are using sysinit and not systemd, you might want to edit ts3 startup script. use these 3 lines, or just manually edit it (change top lines only)
-#wget --no-check-certificate https://raw.githubusercontent.com/Flegma/ts3_startscript_edits/master/ts3_initscript_edit.txt && mv ts3_initscript_edit.txt /home/flegma/teamspeak/ts3_init_edit.txt
-#sed -i '1,3d' /home/flegma/teamspeak/ts3server_startscript.sh
-#tmp="$(mktemp)" && cat /home/flegma/teamspeak/ts3_init_edit.txt /home/flegma/teamspeak/ts3server_startscript.sh >"$tmp" && mv "$tmp" /home/flegma/teamspeak/ts3server_startscript.sh
-ln -s /home/flegma/teamspeak/ts3server_startscript.sh /etc/init.d/teamspeak
-#update-rc.d teamspeak defaults
-service teamspeak start &>> /home/flegma/teamspeak/teamspeak-output.txt
+#wget --no-check-certificate https://files.teamspeak-services.com/releases/server/3.5.0/teamspeak3-server_linux_amd64-3.5.0.tar.bz2 #we need the --no-check-certificate here because we are using old ssl libs and it will fail to verify ssl certificates
+#tar -xjvf teamspeak3-server_linux_amd64-3.5.0.tar.bz2
+#mv teamspeak3-server_linux_amd64 /home/flegma/teamspeak
+#touch /home/flegma/teamspeak/.ts3server_license_accepted
+##if you are using sysinit and not systemd, you might want to edit ts3 startup script. use these 3 lines, or just manually edit it (change top lines only)
+##wget --no-check-certificate https://raw.githubusercontent.com/Flegma/ts3_startscript_edits/master/ts3_initscript_edit.txt && mv ts3_initscript_edit.txt /home/flegma/teamspeak/ts3_init_edit.txt
+##sed -i '1,3d' /home/flegma/teamspeak/ts3server_startscript.sh
+##tmp="$(mktemp)" && cat /home/flegma/teamspeak/ts3_init_edit.txt /home/flegma/teamspeak/ts3server_startscript.sh >"$tmp" && mv "$tmp" /home/flegma/teamspeak/ts3server_startscript.sh
+#ln -s /home/flegma/teamspeak/ts3server_startscript.sh /etc/init.d/teamspeak
+##update-rc.d teamspeak defaults
+#service teamspeak start &>> /home/flegma/teamspeak/teamspeak-output.txt
 #and now csgo server scripts
 apt-get install dnsutils -y
 wget --no-check-certificate https://raw.githubusercontent.com/crazy-max/csgo-server-launcher/master/install.sh && mv install.sh csgo-install.sh && chmod +x csgo-install.sh && ./csgo-install.sh
